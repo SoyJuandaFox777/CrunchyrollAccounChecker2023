182dbde36e9fc5c72a580092ef4819e11a5ea3be
@Jd_Anony <137781868+SoyJuandaFox777@users.noreply.github.com>
2023-07-05 15:51:44 -0500

    Up

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
-#!/usr/bin/env python3 
- """ 
-        _____          __________   __________   __________   ___________   __________   _________  ___        ___  _________     _______ 
-       /     \        |___    ___| |___    ___| |___    ___| |           | |   _______| |   ___   | \  \      /  / |   ___   |   /  ___  \ 
-      /  / \  \           |  |         |  |         |  |     |  |\   /|  | |  |_______  |  |___|  |  \  \    /  /  |  |___|  |  /  /   \  \ 
-     /  /___\  \          |  |         |  |         |  |     |  | \ / |  | |   _______| |   ______|   \  \  /  /   |      ___| |  |     |  | 
-    /  /_____\  \    __   |  |         |  |         |  |     |  |     |  | |  |         |  |           \  \/  /    |  |\  \    |  |     |  | 
-   /  /       \  \  |  |__|  |         |  |      ___|  |___  |  |     |  | |  |_______  |  |            |    |     |  | \  \    \  \__/   / 
-  /__/         \__\ |________|         |__|     |__________| |__|     |__| |__________| |__|            |____|     |__|  \__\    \_______/ 
- """ 
-  
- ### Importing 
- # Importing Inbuilt-Packages 
- import os 
-  
- # Importing Dev Defined Script 
- import src.checker 
-  
-  
- def main(): 
-  
-     if not os.path.exists('result'): 
-         os.makedirs('result') 
-     filename = input("Enter the name or path of file: ") 
-     if os.path.isfile(filename): 
-         src.checker.CrunchyrollChecker.create(filename) 
-     else: 
-         print("File not found.") 
-  
-  
- ### yeaaahhhh!!!! 
- if __name__ == "__main__": 
-     main()
+#!/usr/bin/env python3
+"""
+       _____          __________   __________   __________   ___________   __________   _________  ___        ___  _________     _______
+      /     \        |___    ___| |___    ___| |___    ___| |           | |   _______| |   ___   | \  \      /  / |   ___   |   /  ___  \
+     /  / \  \           |  |         |  |         |  |     |  |\   /|  | |  |_______  |  |___|  |  \  \    /  /  |  |___|  |  /  /   \  \
+    /  /___\  \          |  |         |  |         |  |     |  | \ / |  | |   _______| |   ______|   \  \  /  /   |      ___| |  |     |  |
+   /  /_____\  \    __   |  |         |  |         |  |     |  |     |  | |  |         |  |           \  \/  /    |  |\  \    |  |     |  |
+  /  /       \  \  |  |__|  |         |  |      ___|  |___  |  |     |  | |  |_______  |  |            |    |     |  | \  \    \  \__/   /
+ /__/         \__\ |________|         |__|     |__________| |__|     |__| |__________| |__|            |____|     |__|  \__\    \_______/
+"""
+
+### Importing
+# Importing Inbuilt-Packages
+import os
+
+# Importing Dev Defined Script
+import src.checker
+
+
+def main():
+    
+    if not os.path.exists('result'):
+        os.makedirs('result')
+    filename = input("Enter the name or path of file: ")
+    if os.path.isfile(filename):
+        src.checker.CrunchyrollChecker.create(filename)
+    else:
+        print("File not found.")
+
+
+### yeaaahhhh!!!!
+if __name__ == "__main__":
+    main()
